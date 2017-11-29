9c2bc04f7667986aa0932c5fb5dc88b1bf58e357
nwahnawe <neivensonia@yahoo.fr>
2017-11-29 16:41:01 +0100

    User DAO

---
+++ Luna_Project/src/main/java/com/formation/Dao/UserDao.java
@@ -0,0 +1,84 @@
+package com.formation.Dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.formation.db.ObjetConnect;
+
+public class UserDao {
+
+	Connection connexion = ObjetConnect.getInstance();
+
+	public ResultSet getAllUser() {
+		try {
+			Statement state = connexion.createStatement();
+			ResultSet resultat = state.executeQuery("select * from utilisateur");
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public ResultSet getUnUtilisateur(int id) {
+		try {
+			Statement state = connexion.createStatement();
+			ResultSet resultat = state.executeQuery("select * from utilisateur where id_user=" + id);
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public int delUser(int id) {
+		try {
+			Statement state = connexion.createStatement();
+			int resultat = state.executeUpdate("DELETE FROM `utilisateur` WHERE id_user=" + id);
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int addUser(int id, String pseudo, String mdp) {
+		try {
+			Statement state = connexion.createStatement();
+			int resultat = state.executeUpdate(
+					"INSERT INTO `utilisateur`(`id_user`,`pseudo`, `mdp`) VALUES (" + id
+							+ ",'" + pseudo + "','" + mdp + ")");
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int updateUser(int id, String pseudo, String mdp) {
+		try {
+			Statement state = connexion.createStatement();
+			int resultat = state.executeUpdate("UPDATE `utilisateur` SET `id`=" + id + ",`pseudo`='"
+					+ pseudo + "',`mdp`=" + mdp+  " WHERE id=" + id);
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+}
