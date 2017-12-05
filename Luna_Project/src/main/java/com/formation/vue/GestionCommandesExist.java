6e4a13902e2e97e6f4c9f61d2bd40e3bed615069
nwahnawe <neivensonia@yahoo.fr>
2017-12-05 13:50:39 +0100

    fenetre commandes existantes

---
+++ Luna_Project/src/main/java/com/formation/vue/GestionCommandesExist.java
@@ -0,0 +1,215 @@
+package com.formation.vue;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class GestionCommandesExist extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GestionCommandesExist frame = new GestionCommandesExist();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GestionCommandesExist() {
+		setTitle("Gestion des Commandes Existantes");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(GestionCommandesExist.class.getResource("/images/Moon-32.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 660);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 228, 181));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 874, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 611, Short.MAX_VALUE)
+		);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 140, 0));
+		
+		JButton button = new JButton("Imprimer");
+		button.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Printer-48.png")));
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Tahoma", Font.BOLD, 11));
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		
+		JButton button_1 = new JButton("Aper\u00E7u");
+		button_1.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Preview-48.png")));
+		button_1.setForeground(Color.WHITE);
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		button_1.setContentAreaFilled(false);
+		button_1.setBorderPainted(false);
+		
+		JButton button_2 = new JButton("Export");
+		button_2.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Data-Export-48.png")));
+		button_2.setForeground(Color.WHITE);
+		button_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		button_2.setContentAreaFilled(false);
+		button_2.setBorderPainted(false);
+		
+		JButton button_3 = new JButton("Acceuil");
+		button_3.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Home-48.png")));
+		button_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		button_3.setContentAreaFilled(false);
+		button_3.setBorderPainted(false);
+		
+		JButton button_4 = new JButton("Supprimer");
+		button_4.setBorder(null);
+		button_4.setContentAreaFilled(false);
+		button_4.setBorderPainted(false);
+		button_4.setBackground(new Color(240, 240, 240));
+		button_4.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Garbage-Open-48.png")));
+		button_4.setForeground(Color.WHITE);
+		button_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		JLabel label = new JLabel("Commandes");
+		label.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/commande/Shopping-Bag-64-actif.png")));
+		label.setFont(new Font("Tahoma", Font.BOLD, 15));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_3)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+								.addComponent(button, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(button_1)))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(19)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addGap(10)
+									.addComponent(button_4))
+								.addComponent(label))))
+					.addContainerGap(20, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(5)
+					.addComponent(label)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(button_4)
+					.addGap(77)
+					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
+					.addGap(1)
+					.addComponent(button, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 159, Short.MAX_VALUE)
+					.addComponent(button_3)
+					.addContainerGap())
+		);
+		panel.setLayout(gl_panel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JButton lblRecherche = new JButton("Recherche");
+		lblRecherche.setContentAreaFilled(false);
+		lblRecherche.setBorderPainted(false);
+		lblRecherche.setIcon(new ImageIcon(GestionCommandesExist.class.getResource("/images/gestion/Search-32.png")));
+		lblRecherche.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		JLabel lblTotalDesCommandes = new JLabel("Total des Commandes");
+		lblTotalDesCommandes.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 196, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 672, Short.MAX_VALUE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addGap(84)
+							.addComponent(lblTotalDesCommandes)
+							.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+							.addComponent(lblNewLabel)
+							.addGap(40)
+							.addComponent(lblRecherche)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 187, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 564, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblRecherche)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblTotalDesCommandes)
+								.addComponent(lblNewLabel)))
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 611, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Code", "Client", "Mode paiement", "Total TTC", "Date"
+			}
+		));
+		scrollPane.setViewportView(table);
+		panel_1.setLayout(gl_panel_1);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
